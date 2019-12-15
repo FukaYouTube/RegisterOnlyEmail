9178ccf76b3a469d7ae54bfb3d275239ab951e01
Fuka <weendibol21300@gmail.com>
2019-12-15 17:28:24 +0600

    url localhosts

---
+++ source/ipconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ipconfig(){
+        let ip = os.networkInterfaces().en0
+        if(ip){
+            return `https://${ip[1].address}:${port}/`
+        }else{ return `https://localhost:${port}/` }
+    }
+}
