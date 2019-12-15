3730a56985af74fa38c04dee324418cce000935a
Fuka <weendibol21300@gmail.com>
2019-12-15 18:51:56 +0600

    update file added more code :)

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require('express')
+// import packages
+const express           = require('express')
+const expressSession    = require('express-session')
+const bodyParser        = require('body-parser')
+
+// application express
 const app = express()
 
+// use all packages, views public folders and set ejs files
+app.set('view engine', 'ejs')
+app.use('/public', express.static('public'))
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }))
+app.use(expressSession({ secret: process.env.SECRET, cookie: { maxAge: null }, resave: false, saveUninitialized: false }))
 
+// import router
+const { authRotuer } = require('./router')
+
+// use router
+app.use('/', authRotuer)
 
 module.exports = app
