4f302de9c7423b2697e984fdcfb173a3ac5fa838
Fuka <weendibol21300@gmail.com>
2019-12-18 11:38:13 +0600

    add get api

--- router/authRotuer.js
+++ router/authRotuer.js
@@ -4,4 +4,7 @@ const authService = require('../service/authService')
 rotuer.get('/', (req, res) => authService.Get(req, res))
 rotuer.post('/', (req, res) => authService.Post(req, res))
 
+// get api
+router.get('/mail/api/:userId&:code', (req, res) => authService.getApi(req, res))
+
 module.exports = rotuer
