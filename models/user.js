5b6afb1fea398f9a7c3ba103d3ccef779d254cb6
Fuka <weendibol21300@gmail.com>
2019-12-18 11:22:13 +0600

    added identification to schemas

--- models/user.js
+++ models/user.js
@@ -2,15 +2,16 @@ const { Schema } = require('mongoose')
 
 let user = new Schema({
     // required
-    email: { type: String, required: true },
-    code: { type: String, required: true },
+    email:  { type: String, required: true },
+    code:   { type: String, required: true },
 
     // user
-    username: String,
+    username:   String,
     first_name: String,
 
     // default
-    date: { type: Date, default: Date.now }
+    id:     { type: String, default: Math.random().toString(36).substr(2, 9) },
+    date:   { type: Date, default: Date.now }
 })
 
 module.exports = user
