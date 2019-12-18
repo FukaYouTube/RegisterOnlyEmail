7b798ffcd4da1a5d049fa33fdfd479a2db810703
Fuka <weendibol21300@gmail.com>
2019-12-18 12:21:51 +0600

    add user service

---
+++ service/userService.js
@@ -0,0 +1,12 @@
+const { User } = require('../models')
+
+exports.getUser = async (req, res) => {
+    let user = await User.findOne({ $or: [{ id: req.params.user }, { username: req.params.user }] })
+    
+    if(user){
+        Number(req.session.userId) === user._id ?
+        res.send(user) : res.status(200).send('ok')
+    }else{
+        res.status(404).send()
+    }
+}
