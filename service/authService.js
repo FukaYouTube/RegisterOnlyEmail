9e1e8f692aaeeda8c104b4dbc8287e475b35a1fd
Fuka <weendibol21300@gmail.com>
2019-12-18 11:26:29 +0600

    update file

--- service/authService.js
+++ service/authService.js
@@ -10,8 +10,14 @@ exports.Post = async (req, res) => {
     let user = await User.findOne({ email: req.body.email })
 
     if(!user){
-        mailService
-    }else{
+        let code = Math.floor(10000000000 + Math.random() * 90000000000) // 11
 
+        user = new User({ email: req.body.email, code })
+        user.save()
+
+        mailService.sendMailToNewUser(req.body.email, code, user)
+    }else{
+        let code = Math.floor(10000000000 + Math.random() * 90000000000) // 11
+        mailService.sendMailTo(req.body.email, code, user)
     }
 }
