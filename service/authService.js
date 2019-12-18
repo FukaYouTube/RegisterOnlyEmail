decc677ce7be808caef234cbfb3833b97bc3610b
Fuka <weendibol21300@gmail.com>
2019-12-18 12:21:28 +0600

    added redirect and update code on db users

--- service/authService.js
+++ service/authService.js
@@ -16,9 +16,16 @@ exports.Post = async (req, res) => {
         user.save()
 
         mailService.sendMailToNewUser(req.body.email, code, user)
+
+        res.redirect('/')
     }else{
         let code = Math.floor(10000000000 + Math.random() * 90000000000) // 11
         mailService.sendMailTo(req.body.email, code, user)
+
+        user.code = code
+        user.save()
+
+        res.redirect('/')
     }
 }
 
@@ -26,8 +33,11 @@ exports.Post = async (req, res) => {
 exports.getApi = async (req, res) => {
     const { userId, code } = req.params
 
+    let user = await User.findOne({ id: userId })
+
     if(code == user.code){
-        let user = await User.findOne({ id: userId })
+        req.session.userId = user._id
+        console.log(req.session)
 
         user.username ?
         res.redirect(`/u/${user.username}`) : res.redirect(`/u/${user.id}`)
