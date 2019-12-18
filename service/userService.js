f8b0df9c0a51e1bc930e70f503ca9b06153dd92b
Fuka <weendibol21300@gmail.com>
2019-12-18 12:25:12 +0600

    comparison user id and session user id

--- service/userService.js
+++ service/userService.js
@@ -4,7 +4,7 @@ exports.getUser = async (req, res) => {
     let user = await User.findOne({ $or: [{ id: req.params.user }, { username: req.params.user }] })
     
     if(user){
-        Number(req.session.userId) === user._id ?
+        req.session.userId == user._id ?
         res.send(user) : res.status(200).send('ok')
     }else{
         res.status(404).send()
